9e4a9148a9a8cfb0afa00474a25a7f5c11a214cc
brooss teambb <brooss.teambb@gmail.com>
2011-03-08 11:32:11 +1100

    VP8Inspector: check file ext

---
+++ src/net/sf/javavp8decoder/tools/VP8InspectorUtils.java
@@ -0,0 +1,19 @@
+package net.sf.javavp8decoder.tools;
+
+import java.io.File;
+
+public class VP8InspectorUtils {
+    /*
+     * Get the extension of a file.
+     */  
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
+}
